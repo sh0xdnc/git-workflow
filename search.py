fd28dac98d45b71ac6c19b59d7cdf44274a1b78b
sh0xdnc <sh0xdnc@gmail.com>
2024-02-13 15:38:51 +0100

    added search function

--- search.py
+++ search.py
@@ -1,2 +1,11 @@
 import pandas as pd
 
+print('Enter search string')
+
+uinput = input()
+
+df = pd.read_excel('test.xlsx')
+
+fdf = df[df['name'].str.contains(uinput)]
+
+fdf.to_excel('output.xlsx', sheet_name='sheet1', index=False)
